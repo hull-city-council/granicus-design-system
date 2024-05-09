bfac79224bdd76c9403cc912330705f915c20d18
hccdanny <117913283+HCCDanny@users.noreply.github.com>
2024-05-09 14:37:15 +0000

    increased size

--- src/FormAlert.jsx
+++ src/FormAlert.jsx
@@ -9,27 +9,27 @@ export default function FormAlert({ ...props }) {
   function getIcon(type) {
     switch (type) {
       case "success":
-        return <CheckCircleIcon />;
+        return <CheckCircleIcon sx={{ fontSize: 32 }} />;
       case "warning":
-        return <WarningIcon />;
+        return <WarningIcon sx={{ fontSize: 32 }} />;
       case "danger":
-        return <ReportIcon />;
+        return <ReportIcon sx={{ fontSize: 32 }} />;
       case "neutral":
-        return <InfoIcon />;
+        return <InfoIcon sx={{ fontSize: 32 }} />;
     }
   }
   return (
     <>
       <Alert
         key={props.title}
-        sx={{ alignItems: "flex-start" }}
+        sx={{ alignItems: "flex-start", p: 4 }}
         startDecorator={getIcon(props.type)}
         variant="soft"
         size="lg"
         color={props.type}
       >
         <div>
-          <Typography level="h5">{props.title}</Typography>
+          <Typography level="h3">{props.title}</Typography>
           <Typography level="body-md" color={props.type}>
             {props.content}
           </Typography>
