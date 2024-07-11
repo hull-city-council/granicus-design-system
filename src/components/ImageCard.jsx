31ae58865ef6b3f4ebe10682a1aef23f4d33cb03
hccdanny <117913283+HCCDanny@users.noreply.github.com>
2024-07-11 16:12:57 +0000

    Slider card. Needs work.

--- src/components/ImageCard.jsx
+++ src/components/ImageCard.jsx
@@ -1,17 +1,22 @@
 import React from "react";
-import { Card, CardContent, Typography, Button, Grid, Box } from "@mui/joy";
+import {
+  Card,
+  CardContent,
+  Typography,
+  Button,
+  Grid,
+  CardCover,
+} from "@mui/joy";
 import MobileStepper from "@mui/material/MobileStepper";
 import Paper from "@mui/material/Paper";
 import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
 import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
 import SwipeableViews from "react-swipeable-views";
-import { autoPlay } from "react-swipeable-views-utils";
+import { bindKeyboard } from "react-swipeable-views-utils";
 import { useTheme } from "@mui/joy/styles";
 
 export default function ImageCard({ ...props }) {
-  const AutoPlaySwipeableViews = autoPlay(SwipeableViews, [
-    { interval: 10000 },
-  ]);
+  const AutoPlaySwipeableViews = bindKeyboard(SwipeableViews);
 
   const images = [
     {
@@ -42,143 +47,122 @@ export default function ImageCard({ ...props }) {
 
   return (
     <>
-      <Card
-        variant="plain"
-        sx={(theme) => ({
-          boxShadow: theme.shadow.md,
-          width: "100%",
-          flexGrow: 1,
-          minHeight: 200,
-          backgroundImage:
-            "url('https://fs-filestore-eu.s3.eu-west-1.amazonaws.com/hull/images/myaccount-bg.svg')",
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        })}
-      >
-        {" "}
-        <CardContent sx={{ justifyContent: "center" }}>
-          <Grid container spacing={1}>
-            <Grid xs={6}>
-              <Typography level="h2" fontWeight={"normal"} sx={{ mb: 0 }}>
-                {props.accountname},
-              </Typography>
-              <Typography level="h1" fontWeight={"normal"} sx={{ mt: 0 }}>
-                Welcome to{" "}
-                <Typography level="h1" sx={{ fontWeight: "bold" }}>
-                  myAccount
+      <Grid container spacing={2} sx={{ flexGrow: 1 }}>
+        <Grid xs={6}>
+          <Card
+            variant="plain"
+            sx={(theme) => ({
+              boxShadow: theme.shadow.md,
+              width: "100%",
+              flexGrow: 1,
+              minHeight: 280,
+              backgroundImage:
+                "url('https://fs-filestore-eu.s3.eu-west-1.amazonaws.com/hull/images/myaccount-bg.svg')",
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            })}
+          >
+            {" "}
+            <CardContent sx={{ justifyContent: "center" }}>
+              <Grid spacing={1}>
+                <Typography level="h2" fontWeight={"normal"} sx={{ mb: 0 }}>
+                  {props.accountname},
                 </Typography>
+                <Typography level="h1" fontWeight={"normal"} sx={{ mt: 0 }}>
+                  Welcome to{" "}
+                  <Typography level="h1" sx={{ fontWeight: "bold" }}>
+                    myAccount
+                  </Typography>
+                </Typography>
+                <Grid container spacing={1} sx={{ mt: 2, width: "100%" }}>
+                  <Grid>
+                    <Button
+                      variant="solid"
+                      color="danger"
+                      size="lg"
+                      component="a"
+                      href="/MyRequests"
+                    >
+                      Your requests
+                    </Button>
+                  </Grid>
+                  <Grid>
+                    <Button
+                      color="danger"
+                      size="lg"
+                      variant="soft"
+                      component="a"
+                      href="/MyServices"
+                    >
+                      All forms
+                    </Button>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </CardContent>
+          </Card>
+        </Grid>
+        <Grid xs={6}>
+          <Card sx={{ minHeight: "280px", width: 320 }}>
+            <CardCover>
+              <img
+                src={images[activeStep].imgPath}
+                srcSet={images[activeStep].imgPath}
+                loading="lazy"
+                alt=""
+              />
+            </CardCover>
+            <CardCover
+              sx={{
+                background:
+                  "linear-gradient(to top, rgba(0,0,0,0.4), rgba(0,0,0,0) 200px), linear-gradient(to top, rgba(0,0,0,0.8), rgba(0,0,0,0) 300px)",
+              }}
+            />
+            <CardContent sx={{ justifyContent: "flex-end" }}>
+              <Typography level="title-lg" textColor="#fff">
+                {images[activeStep].label}
               </Typography>
-            </Grid>
-            <Grid xs={6} sx={{ mb: -25 }}>
-              <Box
-                sx={(theme) => ({
-                  boxShadow: theme.shadow.md,
-                  maxWidth: 450,
-                  flexGrow: 1,
-                })}
-              >
-                <Paper
-                  square
-                  elevation={0}
-                  sx={{
-                    display: "flex",
-                    alignItems: "center",
-                    height: 50,
-                    pl: 2,
-                    bgcolor: "background.default",
-                  }}
-                >
-                  <Typography>{images[activeStep].label}</Typography>
-                </Paper>
-                <AutoPlaySwipeableViews
-                  axis={theme.direction === "rtl" ? "x-reverse" : "x"}
-                  index={activeStep}
-                  onChangeIndex={handleStepChange}
-                  enableMouseEvents
-                >
-                  {images.map((step, index) => (
-                    <div key={step.label}>
-                      {Math.abs(activeStep - index) <= 2 ? (
-                        <Box
-                          component="img"
-                          sx={{
-                            height: 200,
-                            display: "block",
-                            maxWidth: 500,
-                            overflow: "hidden",
-                            width: "100%",
-                          }}
-                          src={step.imgPath}
-                          alt={step.label}
-                        />
-                      ) : null}
-                    </div>
-                  ))}
-                </AutoPlaySwipeableViews>
-                <MobileStepper
-                  steps={maxSteps}
-                  position="static"
-                  activeStep={activeStep}
-                  nextButton={
-                    <Button
-                      size="small"
-                      onClick={handleNext}
-                      color="danger"
-                      disabled={activeStep === maxSteps - 1}
-                    >
-                      Next
-                      {theme.direction === "rtl" ? (
-                        <KeyboardArrowLeft />
-                      ) : (
-                        <KeyboardArrowRight />
-                      )}
-                    </Button>
-                  }
-                  backButton={
-                    <Button
-                      size="small"
-                      color="danger"
-                      onClick={handleBack}
-                      disabled={activeStep === 0}
-                    >
-                      {theme.direction === "rtl" ? (
-                        <KeyboardArrowRight />
-                      ) : (
-                        <KeyboardArrowLeft />
-                      )}
-                      Back
-                    </Button>
-                  }
-                />
-              </Box>
-            </Grid>
-          </Grid>
-          <Grid container spacing={1} sx={{ mt: 2, width: "100%" }}>
-            <Grid>
-              <Button
-                variant="solid"
-                color="danger"
-                size="lg"
-                component="a"
-                href="/MyRequests"
-              >
-                Your requests
-              </Button>
-            </Grid>
-            <Grid>
-              <Button
-                color="danger"
-                size="lg"
-                variant="soft"
-                component="a"
-                href="/MyServices"
-              >
-                All forms
-              </Button>
-            </Grid>
-          </Grid>
-        </CardContent>
-      </Card>
+              <MobileStepper
+                sx={{ backgroundColor: "transparent", mt: -3 }}
+                variant="text"
+                steps={maxSteps}
+                position="static"
+                activeStep={activeStep}
+                nextButton={
+                  <Button
+                    size="small"
+                    onClick={handleNext}
+                    color="danger"
+                    disabled={activeStep === maxSteps - 1}
+                  >
+                    Next
+                    {theme.direction === "rtl" ? (
+                      <KeyboardArrowLeft />
+                    ) : (
+                      <KeyboardArrowRight />
+                    )}
+                  </Button>
+                }
+                backButton={
+                  <Button
+                    size="small"
+                    color="danger"
+                    onClick={handleBack}
+                    disabled={activeStep === 0}
+                  >
+                    {theme.direction === "rtl" ? (
+                      <KeyboardArrowRight />
+                    ) : (
+                      <KeyboardArrowLeft />
+                    )}
+                    Back
+                  </Button>
+                }
+              />
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
     </>
   );
 }
